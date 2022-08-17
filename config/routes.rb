0363ee84ad800c062815496e4ddc9f747fbdb1fc
Lorenzo <lorenzodc@macbookLorenzo-2.local>
2022-08-17 11:04:54 +0200

    lecture done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  resources :restaurants do 
+    collection do 
+      get "top"
+    end
+    member do
+      get "chef"
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
+  # reviews/new
+  # restaurants/top
+  # restaurants/:id/chef
+
+  # reviews/2
+  # url to see the form to leave a review on a restaurant
 end
